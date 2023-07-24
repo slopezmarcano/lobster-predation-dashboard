f687f5a3f766e8d15bdf6c21dd9b6fea72fef9cb
Sebastian Lopez-Marcano <slopezmarcano@gmail.com>
2023-07-24 10:43:25 +1000

    Turn the About tab into a box that is always collapsed Fixes #1

--- app.R
+++ app.R
@@ -7,7 +7,7 @@ suppressPackageStartupMessages(library(ggridges))
 
 #-- UI -------------------------------------------------------------------------#
 ui <- dashboardPage(
-  skin = "yellow",
+  skin = "purple",
   dashboardHeader(title = "🦞Lobster Predation Behaviour"), 
   dashboardSidebar(
     # Add a text box with key explanation
@@ -33,31 +33,39 @@ ui <- dashboardPage(
   ),
   dashboardBody(
     # About tab content
-    tabItem(tabName = "about",
-            h1("💡About"),
-            p("This research project focuses on understanding the predation behavior between lobsters and sea urchins in Tasmanian waters, 
-            which are experiencing significant warming and species redistribution. The long-spined sea urchin, Centrostephanus rodgersii, 
-            has extended its range in Tasmanian coastal waters, leading to over-grazing and unproductive barren habitats."),
-            p("Rock lobsters are important predators of sea urchins, with the eastern rock lobster being a significant predator of C. rodgersii in its natural range. 
-            However, eastern rock lobsters are uncommon in Tasmania. This research aims to test the relative predation of urchins bythe eastern rock lobster"),
-            p("This dashboard allows you to explore the data collected from the experiments and visualise the results."),
-            p("@slopezmarcano and @jesmith5")
-            # Add an image for the "About" section that is located in the assets folder
-            #img(src = "https://www.dpi.nsw.gov.au/__data/assets/image/0018/117540/lobster.jpg", height = 200, width = 200, align = "center")
-    ), 
+    fluidRow(
+            box(
+              title = "About",
+              status = "success",
+              solidHeader = TRUE,
+              width = 12,
+              collapsible = TRUE,
+              collapsed = TRUE,
+              #h1("💡About"),
+              p("This research project focuses on understanding the predation behavior between lobsters and sea urchins in Tasmanian waters, 
+                which are experiencing significant warming and species redistribution. The long-spined sea urchin, Centrostephanus rodgersii, 
+                has extended its range in Tasmanian coastal waters, leading to over-grazing and unproductive barren habitats."),
+              p("Rock lobsters are important predators of sea urchins, with the eastern rock lobster being a significant predator of C. rodgersii in its natural range. 
+                However, eastern rock lobsters are uncommon in Tasmania. This research aims to test the relative predation of urchins by the eastern rock lobster"),
+              p("This dashboard allows you to explore the data collected from the experiments and visualize the results."),
+              p("@slopezmarcano and @jesmith5")
+              # Add an image for the "About" section that is located in the assets folder
+              # img(src = "https://www.dpi.nsw.gov.au/__data/assets/image/0018/117540/lobster.jpg", height = 200, width = 200, align = "center")
+            )
+),
     # Add a tab for all the plots
-    tabItem(tabName = 'visualisations',
-            h1("📊Visualisations"),
             fluidRow(
-              #column(width = 12, h4('No of Urchins in Event_ID')),column(width = 12, 
-              box(collapsible = TRUE, status = "warning", title= 'No of Urchins in Event_ID', verbatimTextOutput("max_urchin_count_output"), height = 150)),
+              box(width = 12, title = 'Descriptive Statistics', collapsible = TRUE, status = "success",
+              infoBoxOutput("max_urchin_count_output", width = 4),
+              infoBoxOutput("median_urchin_count_output", width = 4),
+              infoBoxOutput("possible_predation_output", width = 4))),
             fluidRow(
               column(width=12, h4("Predation Motifs in Event_ID")),
-              box(collapsible = TRUE, status = "warning",  title="Time when Motifs ocurred",  plotOutput("time_motif_plot", height = 300)),
-              box(collapsible = TRUE, status = "warning", title="Number of Motifs in Event_ID", plotOutput("motif_count_plot", height = 300))),
+              box(collapsible = TRUE, status = "success",  title="Time when Motifs ocurred",  plotOutput("time_motif_plot", height = 300)),
+              box(collapsible = TRUE, status = "success", title="Number of Motifs in Event_ID", plotOutput("motif_count_plot", height = 300))),
             fluidRow(
-              column(width=12, h4("Behavioural Sequences Across Predation Motifs")),
-              column(width=12, plotOutput("interaction_plot", height = 600))))))
+              column(width=12, h4("Behavioural Sequences Across Predation Motifs > 20 frames")),
+              column(width=12, plotOutput("interaction_plot", height = 600)))))
 
 
 #-- Server ---------------------------------------------------------------------#
@@ -67,14 +75,13 @@ server <- function(input, output, session) {
   observe({
     selected_tank <- input$tank_selection
     event_ids <- switch(selected_tank,
-      "Tank 1" = c("1_N1","1_N11","1_N12","1_N13","1_N14","1_N15","1_N4","1_N5","1_N6","1_N7","1_N8","1_N9"),
-      "Tank 2" = c("2_N1","2_N10","2_N11","2_N12","2_N13","2_N14","2_N15","2_N3","2_N4","2_N5","2_N6","2_N8","2_N9"),
-      "Tank 3" = c("3_N1","3_N10","3_N11","3_N13","3_N15","3_N3","3_N4","3_N5","3_N6","3_N7","3_N8","3_N9"),
-      "Tank 4" = c("4_N1","4_N10", "4_N12", "4_N13", "4_N15", "4_N3", "4_N4", "4_N5", "4_N6","4_N7","4_N8","4_N9"),
-      "Tank 5" = c("5_N10", "5_N11", "5_N12", "5_N13","5_N14","5_N15","5_N2","5_N4","5_N5"),
-      "Tank 6" = c("6_N10"),  # Replace with appropriate event IDs for Tank 6
-      "Tank 7" = c("7_N10")   # Replace with appropriate event IDs for Tank 7
-    )
+      "Tank 1" = c("1_N1", "1_N4", "1_N5", "1_N6", "1_N7", "1_N8", "1_N9", "1_N11", "1_N12", "1_N13", "1_N14", "1_N15"),
+      "Tank 2" = c("2_N1", "2_N3", "2_N4", "2_N5", "2_N6", "2_N8", "2_N9", "2_N10", "2_N11", "2_N12", "2_N13", "2_N14", "2_N15"),
+      "Tank 3" = c("3_N1", "3_N3", "3_N4", "3_N5", "3_N6", "3_N7", "3_N8", "3_N9", "3_N10", "3_N11", "3_N13", "3_N15"),
+      "Tank 4" = c("4_N1", "4_N3", "4_N4", "4_N5", "4_N6", "4_N7", "4_N8", "4_N9", "4_N10", "4_N12", "4_N13", "4_N15"),
+      "Tank 5" = c("5_N2", "5_N4", "5_N5", "5_N6", "5_N10", "5_N11", "5_N12", "5_N13", "5_N14", "5_N15"),
+      "Tank 6" = c("6_N3", "6_N4", "6_N5", "6_N6", "6_N7", "6_N8", "6_N9", "6_N10", "6_N11", "6_N12", "6_N14", "6_N15"), 
+      "Tank 7" = c("7_N3", "7_N5", "7_N7", "7_N9", "7_N10", "7_N11", "7_N12", "7_N13", "7_N14", "7_N15"))
     updateSelectInput(session, "event_id", choices = event_ids)
   })
 
@@ -146,10 +153,17 @@ server <- function(input, output, session) {
     scale_shape_manual(values = c(0, 2), labels = c("Urchin", "Lobster"))
   })
 
-output$max_urchin_count_output <- renderPrint({
-    max_urchin_count <- filtered_data() %>% filter(category_id=='0') %>% group_by(correct_frame) %>% count() %>% pull(n) %>% max()
-    paste("The urchin MaxN in", input$event_id, "is", max_urchin_count)
+output$max_urchin_count_output <- renderInfoBox({
+    infoBox("Urchin MaxN", filtered_data() %>% filter(category_id == '0') %>% group_by(correct_frame) %>% count() %>% pull(n) %>% max(), icon = icon("calculator"), color="purple")
   })
+
+output$median_urchin_count_output <- renderInfoBox({
+    infoBox("Urchin Median", filtered_data() %>% filter(category_id == '0') %>% group_by(correct_frame) %>% count() %>% pull(n) %>% median(), icon = icon("calculator"), color="purple")
+  })
+
+output$possible_predation_output <- renderInfoBox({
+  infoBox("Possible Predation", ifelse(filtered_data() %>% group_by(group) %>% summarise(n = n()) %>% nrow() > 5, "Yes", "No") , icon = icon("utensils"), color="purple")
+})
 
 output$motif_count_plot <- renderPlot({
   # Access the filtered data using filtered_data()
@@ -157,8 +171,8 @@ output$motif_count_plot <- renderPlot({
 
   # Perform ggplot and show the count of frames for each motif in the selected event_ID
   ggplot(filtered_data %>% group_by(group) %>% count(), aes(x = factor(group), y = n)) +
-  geom_segment(aes(x = factor(group), xend = factor(group), y = 0, yend = n), color = "#264653") +
-  geom_point(size = 4, color = "#e9c46a") +
+  geom_segment(aes(x = factor(group), xend = factor(group), y = 0, yend = n), color = "#631fa6") +
+  geom_point(size = 4, color = "#631fa6") +
   labs(color = "Species") +
   xlab("Motif Number") +
   ylab("Number of Frames") +
@@ -171,7 +185,7 @@ output$time_motif_plot <- renderPlot({
   filtered_data <- filtered_data()
 
   ggplot(filtered_data, aes(x=factor(group), y=minute)) + 
-    geom_boxplot(alpha=0.3, fill="#e76f51") +
+    geom_boxplot(alpha=0.3, fill="#631fa6") +
     theme(legend.position="none") +
     xlab("Motif Number") +
     ylab("Start and End (minutes)")+
